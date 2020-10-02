08175f67fa42fd6f907c89f99af45f2cdc8817cf
César Gómez Gómez <55448868+CGGomez@users.noreply.github.com>
2020-10-02 21:29:01 +0200

    Add files via upload

---
+++ UD2.EjerciciosVarios/Ej2.js
@@ -0,0 +1,8 @@
+var texto; //Ej 2 (varios) César Gómez DAW 2 
+texto = "ejemplo"; //Declaramos e inicializamos la variable
+
+var x; //añadimos una variable más
+
+for(x in texto){
+    console.log(texto[x]); //Recorre el texto letra a letra
+} //imprime una letra por cada diferene espacio correspondiente.
